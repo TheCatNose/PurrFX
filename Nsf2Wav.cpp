549dde5ceb8e8a0569948d20dc991c92feb88d57
CatNose <69460782+TheCatNose@users.noreply.github.com>
2020-08-13 16:38:19 +0300

    Nsf2Wav project - the use of class PurrFX::CNesGme
    It make the code a lot more clean

--- Nsf2Wav.cpp
+++ Nsf2Wav.cpp
@@ -1,7 +1,5 @@
-﻿#include "gme/Music_Emu.h"
-
-#include <iostream>
-#include <memory>
+﻿#include <iostream>
+#include "lib/CNesGme.h"
 
 void showErrorMessage(const char* i_sMessage)
 {
@@ -24,47 +22,20 @@ int main()
 	// Setup emulation //
 	/////////////////////
 
-	gme_err_t sError = nullptr;
-
-	// Determine file type
-	gme_type_t pFileType;
-	sError = gme_identify_file(sFileName, &pFileType);
-	if (sError != nullptr || pFileType == nullptr || pFileType->system != "Nintendo NES")
+	PurrFX::CNesGme oNes;
+	if (!oNes.setSoundOptions(nSampleRate))
 	{
-		showErrorMessage(sError);
-		showErrorMessage("Wrong file type");
+		showErrorMessage("Can't set sound options");
 		return 1;
 	}
-	
-	// Create emulator
-	std::unique_ptr<Music_Emu> pEmu(pFileType->new_emu());
-	if (pEmu == nullptr)
+	if (!oNes.open(sFileName))
 	{
-		showErrorMessage("Out of memory");
+		showErrorMessage("Can't open file");
 		return 1;
 	}
-
-	// Set sample rate
-	sError = pEmu->set_sample_rate(nSampleRate);
-	if (sError != nullptr)
+	if (!oNes.setTrack(nTrack))
 	{
-		showErrorMessage(sError);
-		return 1;
-	}
-
-	// Load music file
-	sError = pEmu->load_file(sFileName);
-	if (sError != nullptr)
-	{
-		showErrorMessage(sError);
-		return 1;
-	}
-
-	// Start track
-	sError = pEmu->start_track(nTrack);
-	if (sError != nullptr)
-	{
-		showErrorMessage(sError);
+		showErrorMessage("Can't set track");
 		return 1;
 	}
 
@@ -74,65 +45,66 @@ int main()
 
 	FILE* pFile = nullptr;
 	fopen_s(&pFile, "data/out/out.wav", "wb");
-	if (pFile != nullptr)
+	if (pFile == nullptr)
 	{
-		// WAV header
+		showErrorMessage("Can't create output file");
+		return 1;
+	}
 
-		const uint32_t nDataSize = nTime * nSampleRate /* stereo */ * 2 /* bytes per sample */ * 2;
-		const int nHdrSize = 44;
-		fwrite("RIFF", 1, 4, pFile);
-		const uint32_t nFileSize = nDataSize + nHdrSize;
-		fwrite(&nFileSize, 1, sizeof(nFileSize), pFile);
-		fwrite("WAVE", 1, 4, pFile);
-		fwrite("fmt ", 1, 4, pFile);
-		const uint32_t nFmtChunkSize = 16;
-		fwrite(&nFmtChunkSize, 1, sizeof(nFmtChunkSize), pFile);
-		const uint16_t nFormat = 1; // PCM
-		fwrite(&nFormat, 1, sizeof(nFormat), pFile);
-		uint16_t nChannels = 2; // Stereo
-		fwrite(&nChannels, 1, sizeof(nChannels), pFile);
-		uint32_t nSampleRate_ = nSampleRate;
-		fwrite(&nSampleRate_, 1, sizeof(nSampleRate_), pFile);
-		uint16_t nBitsPerSample = 16;
-		uint32_t nBitRate = (nSampleRate * nBitsPerSample * nChannels) / 8;
-		fwrite(&nBitRate, 1, sizeof(nBitRate), pFile);
-		uint16_t nBytesPerSample = (nBitsPerSample * nChannels) / 8;
-		fwrite(&nBytesPerSample, 1, sizeof(nBytesPerSample), pFile);
-		fwrite(&nBitsPerSample, 1, sizeof(nBitsPerSample), pFile);
-		fwrite("data", 1, 4, pFile);
-		fwrite(&nDataSize, 1, sizeof(nDataSize), pFile);
+	// WAV header
 
-		uint32_t nBytesToWrite = nDataSize;
+	const uint32_t nDataSize = nTime * nSampleRate /* stereo */ * 2 /* bytes per sample */ * 2;
+	const int nHdrSize = 44;
+	fwrite("RIFF", 1, 4, pFile);
+	const uint32_t nFileSize = nDataSize + nHdrSize;
+	fwrite(&nFileSize, 1, sizeof(nFileSize), pFile);
+	fwrite("WAVE", 1, 4, pFile);
+	fwrite("fmt ", 1, 4, pFile);
+	const uint32_t nFmtChunkSize = 16;
+	fwrite(&nFmtChunkSize, 1, sizeof(nFmtChunkSize), pFile);
+	const uint16_t nFormat = 1; // PCM
+	fwrite(&nFormat, 1, sizeof(nFormat), pFile);
+	uint16_t nChannels = 2; // Stereo
+	fwrite(&nChannels, 1, sizeof(nChannels), pFile);
+	uint32_t nSampleRate_ = nSampleRate;
+	fwrite(&nSampleRate_, 1, sizeof(nSampleRate_), pFile);
+	uint16_t nBitsPerSample = 16;
+	uint32_t nBitRate = (nSampleRate * nBitsPerSample * nChannels) / 8;
+	fwrite(&nBitRate, 1, sizeof(nBitRate), pFile);
+	uint16_t nBytesPerSample = (nBitsPerSample * nChannels) / 8;
+	fwrite(&nBytesPerSample, 1, sizeof(nBytesPerSample), pFile);
+	fwrite(&nBitsPerSample, 1, sizeof(nBitsPerSample), pFile);
+	fwrite("data", 1, 4, pFile);
+	fwrite(&nDataSize, 1, sizeof(nDataSize), pFile);
 
-		// WAV data
+	uint32_t nBytesToWrite = nDataSize;
 
-		while (nBytesToWrite > 0)
+	// WAV data
+
+	while (nBytesToWrite > 0)
+	{
+		// Prepare buffer
+		const uint32_t nBufferSize = 1024;
+		char           aBuffer[nBufferSize];
+
+		// Fill buffer
+		if (!oNes.render(aBuffer, nBufferSize))
 		{
-			// Prepare buffer
-			const uint32_t nBufferSize = 1024;
-			short          aBuffer[nBufferSize];
-			const uint32_t nBufferBytes = nBufferSize * sizeof(aBuffer[0]);
-
-			// Fill buffer
-			sError = pEmu->play(nBufferSize, aBuffer);
-			if (sError != nullptr)
-			{
-				showErrorMessage(sError);
-				break;
-			}
-
-			// Write to file
-			uint32_t nSize = nBufferBytes;
-			if (nSize >= nBytesToWrite)
-				nSize  = nBytesToWrite;
-			fwrite(aBuffer, 1, nSize, pFile);
-			nBytesToWrite -= nSize;
+			showErrorMessage("Rendering error");
+			fclose(pFile);
+			return 1;
 		}
 
-		fclose(pFile);
+		// Write to file
+		uint32_t nSize = nBufferSize;
+		if (nSize >= nBytesToWrite)
+			nSize  = nBytesToWrite;
+		fwrite(aBuffer, 1, nSize, pFile);
+		nBytesToWrite -= nSize;
 	}
-	else
-		return 1;
 
+	fclose(pFile);
+
+	std::cout << "Success!" << std::endl;
 	return 0;
 }
