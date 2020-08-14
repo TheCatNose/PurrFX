93baa98482e4a5396d45d877c98838f30e7ba1ba
CatNose <69460782+TheCatNose@users.noreply.github.com>
2020-08-14 22:47:48 +0300

    PurrFX - definitions for classes - disabling default constructor

--- lib/DClass.h
+++ lib/DClass.h
@@ -4,3 +4,6 @@
     className& operator=(const className&) = delete; \
     className(const className&) = delete; \
     className() = default;
+
+#define CLASS_DISABLE_DEFAULT_CONSTRUCTOR(className) \
+    className() = delete;
