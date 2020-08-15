25d24744c629b1310b728968bafa36949e8270c2
CatNose <69460782+TheCatNose@users.noreply.github.com>
2020-08-15 15:22:38 +0300

    PurrFX::CNesLogItem - requirement of type function

--- lib/CNesLogItem.h
+++ lib/CNesLogItem.h
@@ -1,11 +1,14 @@
 #pragma once
 // Base class for every type of log item/event
 
+#include "ENesLogItemType.h"
+
 namespace PurrFX
 {
 	class CNesLogItem
 	{
 	public:
 		virtual ~CNesLogItem();
+		virtual ENesLogItemType type() const = 0;
 	};
 }
