6a9462a258c2088defb7f220784616f3e40c174c
CatNose <69460782+TheCatNose@users.noreply.github.com>
2020-08-22 14:48:35 +0300

    PurrFX - main include file

---
+++ purrfx/PurrFX.h
@@ -0,0 +1,7 @@
+#pragma once
+// Main include file
+
+#include "purrfx/CNesGme.h"
+#include "purrfx/CAudioDataConsumerWavWriter.h"
+#include "purrfx/CAudioDataConsumerDummy.h"
+#include "purrfx/CLogFileWriter.h"
