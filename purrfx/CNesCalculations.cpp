67f998eab4171fea6c69e1c8b615fe71e53d3c52
CatNose <69460782+TheCatNose@users.noreply.github.com>
2020-08-30 19:11:42 +0300

    PurrFX::CNesCalculations - DPCM data length functions

--- purrfx/CNesCalculations.cpp
+++ purrfx/CNesCalculations.cpp
@@ -23,3 +23,26 @@ uint8_t PurrFX::CNesCalculations::dpcmSampleLengthPack(uint16_t i_nPackedLength)
 	// LLLL.LLLL0001
 	return uint8_t( (i_nPackedLength & 0b111111110000) >> 4 );
 }
+
+bool PurrFX::CNesCalculations::isDpcmSampleLengthCorrect(size_t i_nSize)
+{
+	if (i_nSize < dpcmSampleLengthUnpack(0x00) ||
+		i_nSize > dpcmSampleLengthUnpack(0xFF) )
+		return false;
+
+	// %LLLL.LLLL0001 -> 0x01, 0x11, 0x21... 0xFF1
+	if ( (i_nSize & 0x01) != 0x01)
+		return false;
+
+	return true;
+}
+
+uint16_t PurrFX::CNesCalculations::closestSmallerDpcmDataLength(size_t i_nSize)
+{
+	assert(i_nSize > 0);
+
+	if ((i_nSize & 0x1) == 0)
+		 i_nSize -= 0x10;
+
+	return (i_nSize & 0xFF0) + 1;
+}
--- purrfx/CNesCalculations.h
+++ purrfx/CNesCalculations.h
@@ -2,6 +2,7 @@
 // NES hardware related calculations
 
 #include <cstdint>
+#include <cassert>
 
 namespace PurrFX
 {
@@ -16,5 +17,9 @@ namespace PurrFX
 		static uint8_t  dpcmSampleAddressPack(uint16_t i_nPackedAddress);
 		static uint16_t dpcmSampleLengthUnpack(uint8_t i_nPackedLength);
 		static uint8_t  dpcmSampleLengthPack(uint16_t i_nPackedLength);
+
+		// For unpacked length (real size in bytes)
+		static bool isDpcmSampleLengthCorrect(size_t i_nSize);
+		static uint16_t closestSmallerDpcmDataLength(size_t i_nSize);
 	};
 }
