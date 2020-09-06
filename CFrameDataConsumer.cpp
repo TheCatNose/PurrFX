aba43e8ac810c3c405e77cb220a4cd85a61b6d92
CatNose <69460782+TheCatNose@users.noreply.github.com>
2020-09-06 15:38:52 +0300

    PurrFX::CFrameDataConsumer - possibility to limit its size

--- CFrameDataConsumer.cpp
+++ CFrameDataConsumer.cpp
@@ -1,12 +1,18 @@
 #include "CFrameDataConsumer.h"
 
+PurrFX::CFrameDataConsumer::CFrameDataConsumer(size_t i_nSize):
+	m_nSize(i_nSize)
+{
+}
+
 PurrFX::CFrameDataConsumer::~CFrameDataConsumer()
 {
 }
 
 void PurrFX::CFrameDataConsumer::startFrame()
 {
-	m_bFrameStarted = true;
+	if (!finished())
+		m_bFrameStarted = true;
 }
 
 void PurrFX::CFrameDataConsumer::setRegister(ERegister i_eRegister, uint8_t i_nValue)
@@ -19,10 +25,25 @@ void PurrFX::CFrameDataConsumer::setRegister(ERegister i_eRegister, uint8_t i_nV
 
 void PurrFX::CFrameDataConsumer::endFrame()
 {
-	if (!m_bFrameStarted)
+	if (!m_bFrameStarted || finished())
 		return;
 	m_bFrameStarted = false;
 
 	onFrameDataReady(m_oData);
 	m_oData.reset();
+	if (m_nSize != UnlimitedSize)
+		m_nFramesProcessed++;
+}
+
+bool PurrFX::CFrameDataConsumer::finished() const
+{
+	if (m_nSize == UnlimitedSize)
+		return false;
+	assert(m_nFramesProcessed <= m_nSize);
+	return m_nFramesProcessed == m_nSize; 
+}
+
+size_t PurrFX::CFrameDataConsumer::framesProcessed() const
+{
+	return m_nFramesProcessed;
 }
--- CFrameDataConsumer.h
+++ CFrameDataConsumer.h
@@ -2,23 +2,38 @@
 // Base class for all frame data receivers
 
 #include <cstdint>
+#include <cstddef>
+#include <cassert>
 #include "CFrameData.h"
+#include "DClass.h"
 
 namespace PurrFX
 {
 	class CFrameDataConsumer
 	{
+		CLASS_DISABLE_DEFAULT_CONSTRUCTOR(CFrameDataConsumer)
+
 	public:
+		static const size_t UnlimitedSize = 0;
+
+		CFrameDataConsumer(size_t i_nSize = UnlimitedSize);
 		virtual ~CFrameDataConsumer();
 		
 		void startFrame();
 		void setRegister(ERegister i_eRegister, uint8_t i_nValue);
 		void endFrame();
 
+		bool finished() const;
+
+	protected:
+		size_t framesProcessed() const;
+
 	private:
 		virtual void onFrameDataReady(const CFrameData& i_rFrameData) = 0;
 
 		bool m_bFrameStarted = false;
+		size_t m_nSize = 0;
+		size_t m_nFramesProcessed = 0;
 		CFrameData m_oData;
 	};
 }
