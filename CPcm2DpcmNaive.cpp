8394f67d1f0c51a00d244e8fa264152f6ac5fb88
CatNose <69460782+TheCatNose@users.noreply.github.com>
2020-09-09 23:22:44 +0300

    PurrFX::CPcm2DpcmNaive - Naive PCM->DPCM converter

---
+++ CPcm2DpcmNaive.cpp
@@ -0,0 +1,45 @@
+#include "CPcm2DpcmNaive.h"
+#include "CNesCalculations.h"
+
+PurrFX::CPcm2DpcmNaive::CPcm2DpcmNaive(const std::vector<int8_t>& i_rPcmData):
+	CPcm2Dpcm(i_rPcmData)
+{}
+
+PurrFX::CDpcmSample* PurrFX::CPcm2DpcmNaive::convert() const
+{
+	std::vector<bool> aDpcmBits;
+	aDpcmBits.reserve( pcmSize() );
+
+	bool bIncreaseIfNotChanged = false;
+	int8_t nSignal = 0;
+	for (size_t i = 0; i < pcmSize(); i++)
+	{
+		const int8_t nPcmByte = pcmByte(i);
+		if (nPcmByte == nSignal)
+		{
+			aDpcmBits.push_back(bIncreaseIfNotChanged);
+			bIncreaseIfNotChanged = !bIncreaseIfNotChanged;
+		}
+		else
+			aDpcmBits.push_back(nPcmByte > nSignal);
+		nSignal = nPcmByte;
+	}
+
+	CDpcmSample* pSample = new CDpcmSample(0, CNesCalculations::dpcmSampleLengthPack(dpcmSize()));
+	assert(pSample->size() == dpcmSize());
+
+	uint8_t nDpcmByte = 0;
+	for (size_t i = 0; i < aDpcmBits.size(); i++)
+	{
+		size_t nBit = i%8;
+		if (aDpcmBits[i])
+			nDpcmByte |= (uint8_t(1) << nBit);
+		if (nBit == 7)
+		{
+			pSample->set(i/8, nDpcmByte);
+			nDpcmByte = 0;
+		}
+	}
+
+	return pSample;
+}
---
+++ CPcm2DpcmNaive.h
@@ -0,0 +1,20 @@
+#pragma once
+// Naive PCM->DPCM converter
+//
+// Proper implementation should take into account nonstandard signal level changes
+// (possible signal level changes for 1bit DPCM are +1 and -1)
+
+#include "CPcm2Dpcm.h"
+
+namespace PurrFX
+{
+	class CPcm2DpcmNaive: public CPcm2Dpcm
+	{
+		CLASS_DISABLE_DEFAULT_CONSTRUCTOR(CPcm2DpcmNaive)
+		CLASS_MAKE_NON_COPYABLE(CPcm2DpcmNaive)
+
+	public:
+		CPcm2DpcmNaive(const std::vector<int8_t>& i_rPcmData);
+		virtual CDpcmSample* convert() const;
+	};
+}
