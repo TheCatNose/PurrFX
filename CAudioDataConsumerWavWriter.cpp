a650e5eeafcd03aab68e494bfcb6cf54e77552b1
CatNose <69460782+TheCatNose@users.noreply.github.com>
2020-09-12 14:20:07 +0300

    PurrFX::CAudioDataConsumerWavWriter - using CWavHeader

--- CAudioDataConsumerWavWriter.cpp
+++ CAudioDataConsumerWavWriter.cpp
@@ -1,4 +1,5 @@
 #include "CAudioDataConsumerWavWriter.h"
+#include "CWavHeader.h"
 
 PurrFX::CAudioDataConsumerWavWriter::CAudioDataConsumerWavWriter(const pathstring& i_sFileName, uint32_t i_nDuration):
 	CAudioDataConsumer(i_nDuration),
@@ -15,32 +16,11 @@ void PurrFX::CAudioDataConsumerWavWriter::onStart(const CAudioFormat& i_rAudioFo
 	if (!m_oFile.isOpened())
 		return;
 	
-	uint32_t nDataSize = i_nBytesToProcess;
-
-	// WAV header
-
-	const int nHdrSize = 44;
-	m_oFile.write("RIFF", 4);
-	const uint32_t nFileSize = nDataSize + nHdrSize;
-	m_oFile.write(&nFileSize, sizeof(nFileSize));
-	m_oFile.write("WAVE", 4);
-	m_oFile.write("fmt ", 4);
-	const uint32_t nFmtChunkSize = 16;
-	m_oFile.write(&nFmtChunkSize, sizeof(nFmtChunkSize));
-	const uint16_t nFormat = 1; // PCM
-	m_oFile.write(&nFormat, sizeof(nFormat));
-	uint16_t nChannels = i_rAudioFormat.channels();
-	m_oFile.write(&nChannels, sizeof(nChannels));
-	uint32_t nSampleRate = i_rAudioFormat.sampleRate();
-	m_oFile.write(&nSampleRate, sizeof(nSampleRate));
-	uint16_t nBitsPerSample = i_rAudioFormat.bitDepth();
-	uint32_t nBitRate = (nSampleRate * nBitsPerSample * nChannels) / 8;
-	m_oFile.write(&nBitRate, sizeof(nBitRate));
-	uint16_t nBytesPerSample = (nBitsPerSample * nChannels) / 8;
-	m_oFile.write(&nBytesPerSample, sizeof(nBytesPerSample));
-	m_oFile.write(&nBitsPerSample, sizeof(nBitsPerSample));
-	m_oFile.write("data", 4);
-	m_oFile.write(&nDataSize, sizeof(nDataSize));
+	CWavHeader oHeader(i_nBytesToProcess, i_rAudioFormat);
+	assert(oHeader.isValid());
+	CWavHeader::DataArray aData;
+	oHeader.get(aData);
+	m_oFile.write(aData.data(), aData.size());
 }
 
 void PurrFX::CAudioDataConsumerWavWriter::onData(const char* i_pData, uint32_t i_nSize)
