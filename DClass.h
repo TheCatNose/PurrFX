4950184cf647f11bfa84953d73c32bec730607e9
CatNose <69460782+TheCatNose@users.noreply.github.com>
2020-09-13 19:47:13 +0300

    PurrFX - definitions for classes - assignment of access modifiers

--- DClass.h
+++ DClass.h
@@ -1,11 +1,15 @@
 #pragma once
 
 #define CLASS_MAKE_NON_COPYABLE(className) \
+    private: \
     className& operator=(const className&) = delete; \
     className(const className&) = delete;
 
 #define CLASS_USE_DEFAULT_CONSTRUCTOR(className) \
-    className() = default;
+    public: \
+    className() = default; \
+    private:
 
 #define CLASS_DISABLE_DEFAULT_CONSTRUCTOR(className) \
+    private: \
     className() = delete;
