e25c82a7140b3b3f83e87a17acf475560d176b57
CatNose <69460782+TheCatNose@users.noreply.github.com>
2020-09-20 19:12:03 +0300

    PurrFX::NesConsts - pitch calculations constants

--- DNesConsts.h
+++ DNesConsts.h
@@ -22,5 +22,19 @@ namespace PurrFX
 		static const uint16_t dpcmSampleLengthMax  = 0b111111110001;
 
 		static const int dpcmSampleRate = 33000; // approximate value
+
+		/////////
+		// CPU //
+		/////////
+
+		static const double cpuClockSpeedNTSC = 1789773.0L;
+		static const double cpuClockSpeedPAL  = 1662607.0L;
+
+		///////////
+		// Pitch //
+		///////////
+
+		static const uint16_t apuPulseTimerMax    = 0b11111111111;
+		static const uint16_t apuTriangleTimerMax = 0b11111111111;
 	}
 }
